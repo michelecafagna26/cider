e25801dc721597f8fac0403b7645decc8d33acba
Michele Cafagna <m.cafagna26@gmail.com>
2022-01-25 15:14:34 +0100

    name changed

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cider",
+    name="cidereval",
     version="0.0.1",
     author="Michele Cafagna",
     author_email = "michele.cafagna@um.edu.mt",
